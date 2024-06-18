64032f5daca65b055e0202284d557dab9499eb16
Tom Slanda <tomslanda@Toms-Laptop-3.local>
2024-06-18 14:54:18 -0400

    added web page

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-import Fastify from 'fastify';
-import scrape from './scrape.js';
+import Fastify from "fastify";
+import scrape from "./scrape.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import fastifyStatic from "@fastify/static";
 
 const fastify = Fastify({
   logger: true,
 });
 const port = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-fastify.get('/', async (request, reply) => {
-  const htmlContent =
-    '<html lang="en"><head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>MTG Scraper</title> </head> <body> <h1 style="text-align: center">Welcome to MTG Scraper!</h1> <p style="text-align: center">This web app scrapes Magic: The Gathering card prices from eBay sold listings.</p> <p style="text-align: center">To scrape, go to "/scrape".</p> </body></html>';
-  
-  reply.code(200).type('text/html; charset=utf-8').send(htmlContent);
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "pages"),
 });
 
-fastify.get('/scrape', async (request, reply) => {
-  const listings = await scrape('https://www.ebay.com/sch/i.html?_from=R40&_nkw=scrubland+mtg&_sacat=0&LH_Sold=1&LH_Complete=1&LH_PrefLoc=1&_sop=13',
-    'div.s-item__wrapper.clearfix',
-    'div.s-item__title',
-    'span.s-item__price'
-);
-  return reply.send(listings);
+fastify.get("/", function (request, reply) {
+  reply.sendFile("index.html");
+});
 
+fastify.get("/scrape", async (request, reply) => {
+  const listings = await scrape(
+    "https://www.ebay.com/sch/i.html?_from=R40&_nkw=scrubland+mtg&_sacat=0&LH_Sold=1&LH_Complete=1&LH_PrefLoc=1&_sop=13",
+    "div.s-item__wrapper.clearfix",
+    "div.s-item__title",
+    "span.s-item__price"
+  );
+  return reply.send(listings);
 });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: port || 3000, host: '0.0.0.0' });
+    await fastify.listen({ port: port || 3000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
---
+++ pages/app.js
@@ -0,0 +1,54 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () =>
+  getListings("https://mtgscraper-production.up.railway.app/scrape")
+);
+
+/*
+1. Makes a FETCH GET request to the url
+2. Stores the data in local storage as 'listings'
+3. Triggers renderTable function
+*/
+async function getListings(url) {
+  try {
+    const response = await fetch(url);
+    if (!response) {
+      throw new Error("Failed to Fetch");
+    }
+    const data = await response.json();
+    const stringifiedData = JSON.stringify(data);
+    localStorage.setItem("listings", stringifiedData);
+    renderTable();
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+/*
+1. Checks for 'listings' in local storage.  If not found, it returns.
+2. Massages 'listings' data
+3. Loops through 'listings' and creates a table row and cells for each item
+4. Adds CSS styling
+*/
+function renderTable() {
+  if (localStorage.getItem("listings") === null) {
+    console.error("No Data - Please Refetch");
+    return;
+  }
+  const data = localStorage.getItem("listings");
+  const listings = JSON.parse(data);
+  for (i = 0; i < listings.length; i++) {
+    const parent = document.querySelector("#start");
+    const row = document.createElement("tr");
+    const cellOne = document.createElement("td");
+    const cellTwo = document.createElement("td");
+    parent.appendChild(row);
+    row.appendChild(cellOne);
+    row.appendChild(cellTwo);
+    cellOne.innerText = listings[i].title;
+    cellTwo.innerText = listings[i].price;
+    cellOne.className = "border border-black p-2";
+    cellTwo.className = "border border-black p-2";
+  }
+}
+
+renderTable();
