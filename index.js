0e8fb75061595c563933eb8c22a0bf66cc8b3f20
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-06-17 23:47:16 -0400

    made it pretty

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ import scrape from './scrape.js';
 const fastify = Fastify({
   logger: true,
 });
-
 const port = process.env.PORT;
 
-fastify.get('/', async () => {
-  return { hello: 'world' };
+fastify.get('/', async (request, reply) => {
+  const htmlContent =
+    '<h1 style="text-align: center">Welcome to MTG Scraper!</h1><p style="text-align: center">This web app scrapes Magic: The Gathering card prices from eBay sold listings.</p><p style="text-align: center">To scrape, go to "/scrape".</p>';
+  
+  reply.code(200).type('text/html; charset=utf-8').send(htmlContent);
 });
-fastify.get('/scrape', async () => {
-  return scrape('https://www.ebay.com/sch/i.html?_from=R40&_nkw=scrubland+mtg&_sacat=0&LH_Sold=1&LH_Complete=1&LH_PrefLoc=1&_sop=13');
+
+fastify.get('/scrape', async (request, reply) => {
+  const listings = await scrape('https://www.ebay.com/sch/i.html?_from=R40&_nkw=scrubland+mtg&_sacat=0&LH_Sold=1&LH_Complete=1&LH_PrefLoc=1&_sop=13',
+    'div.s-item__wrapper.clearfix',
+    'div.s-item__title',
+    'span.s-item__price'
+);
+  return reply.send(listings);
+
 });
 
 const start = async () => {
--- scrape.js
+++ scrape.js
@@ -1,5 +1,6 @@
 import * as cheerio from 'cheerio';
 
+// Constructor function to create a Listing object
 class Listing {
   constructor(title, price) {
     this.title = title;
@@ -7,7 +8,17 @@ class Listing {
   }
 }
 
-async function scrape(url) {
+/* 
+1. Makes FETCH request to URL
+2. Using Cheerio, creates a data object of the HTML page
+3. Crawls the data object looking for the parentEL
+4. If found and passes the filter, stores the titleEl and priceEL inner text content
+5. Creates a listing object using the titleEl and priceEl inner text content
+6. Pushes listing object to the results array
+7, This repeats for every instance of the parentEl
+8. Returns the first 10 entries of the results array
+*/
+async function scrape(url, parentEl, titleEl, priceEl) {
   try {
     const response = await fetch(url);
     if (!response) {
@@ -16,9 +27,9 @@ async function scrape(url) {
     const html = await response.text();
     const $ = cheerio.load(html);
     const results = [];
-    $('div.s-item__wrapper.clearfix', html).each(function () {
-        const title = $(this).find('div.s-item__title')
-        const price = $(this).find('span.s-item__price')
+    $(parentEl, html).each(function () {
+        const title = $(this).find(titleEl)
+        const price = $(this).find(priceEl)
         if ($(title).text() === "Shop on eBay") {
             return
         }
